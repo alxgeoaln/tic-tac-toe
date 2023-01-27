8c87c9eb90ec448f4085524c680a96aa1fa87bcb
alxgeoaln <georgianalinalexandru@gmail.com>
2023-01-27 19:13:02 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+    },
+    colors: {
+      'green': '#a5c882',
+      'blue': '#5ab1bb',
+      'yellow': '#f7dd72',
+      'dark': "#4E6766"
+    }
+  },
+  plugins: [],
+}
